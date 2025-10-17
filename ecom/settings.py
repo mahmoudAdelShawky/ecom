085270b512cbae1ff830547de968b7e4560ab0c1
mahmoudAdelShawky <mahmoudashawky197@gmail.com>
2025-10-17 08:29:14 +0300

    update settings.py for improved environment

--- ecom/settings.py
+++ ecom/settings.py
@@ -13,13 +13,13 @@ import os
 from pathlib import Path
 import environ
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 # environ.Env.read_env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
